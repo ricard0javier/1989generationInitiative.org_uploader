829a084995302b3d7638c37dd21a82f703e3164c
Ricardo Villanueva <villanueva.ricardo@gmail.com>
2017-01-17 22:03:08 +0000

    Applying doc example

---
+++ src/main/java/org/generationinitiative/uploader/Hello.java
@@ -0,0 +1,11 @@
+package org.generationinitiative.uploader;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Hello implements RequestHandler<String, String> {
+    @Override
+    public String handleRequest(String input, Context context) {
+        return input;
+    }
+}
