75449a141f529d918945896df8f044dc56023cc6
Ricardo Villanueva <villanueva.ricardo@gmail.com>
2017-01-18 22:12:36 +0000

    change aws region to frankfurt and the runtime to java8

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 aws lambda create-function \
-    --region eu-west-1 \
+    --region eu-central-1 \
     --function-name 1989generationinitiative_uploader \
     --zip-file fileb://./build/distributions/1989generationInitiative.org_uploader-1.0.zip \
     --role arn:aws:iam::430132907316:role/initiativeRole \
     --handler org.generationinitiative.uploader.Hello \
-    --runtime nodejs4.3
+    --runtime java8
