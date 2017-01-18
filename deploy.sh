d7f8a451f21fe0c3aef4df42700fd55a10b3de5d
Ricardo Villanueva <villanueva.ricardo@gmail.com>
2017-01-18 22:07:40 +0000

    remove profile from aws cli command

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,4 @@ aws lambda create-function \
     --zip-file fileb://./build/distributions/1989generationInitiative.org_uploader-1.0.zip \
     --role arn:aws:iam::430132907316:role/initiativeRole \
     --handler org.generationinitiative.uploader.Hello \
-    --runtime nodejs4.3 \
-    --profile default \
+    --runtime nodejs4.3
