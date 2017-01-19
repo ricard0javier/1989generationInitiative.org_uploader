d6783ed69ecc96b7c189ecfbc97cb8563f99188b
Ricardo Villanueva <villanueva.ricardo@gmail.com>
2017-01-19 00:30:25 +0000

    provide constructors to help the lambda deserialisation process

--- src/main/java/org/generationinitiative/uploader/dto/RequestDTO.java
+++ src/main/java/org/generationinitiative/uploader/dto/RequestDTO.java
@@ -1,8 +1,12 @@
 package org.generationinitiative.uploader.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RequestDTO {
     private String token;
     private String bucket;
