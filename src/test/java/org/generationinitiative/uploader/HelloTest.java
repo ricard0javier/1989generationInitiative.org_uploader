e5624ee66f30c0ccfb5d115c9f9a066202dbf4c5
Ricardo Villanueva <villanueva.ricardo@gmail.com>
2017-01-21 01:41:22 +0000

    fix test

--- src/test/java/org/generationinitiative/uploader/HelloTest.java
+++ src/test/java/org/generationinitiative/uploader/HelloTest.java
@@ -8,6 +8,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
 public class HelloTest {
 
@@ -26,12 +29,8 @@ public class HelloTest {
     public void test_handleRequest() throws Exception {
 
         System.setProperty("JWT_CLIENT_SECRET", "secret");
-        String request = "{\n" +
-                "    \"token\": null,\n" +
-                "    \"bucket\": null,\n" +
-                "    \"key\": null,\n" +
-                "    \"body\": \"{\\\"token\\\" : \\\"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3JpY2FyZDBqYXZpZXIuZXUuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDU4NzJiMTVjYmY4ZTYzMzI1MDY1OWEwZSIsImF1ZCI6IlIyZ1RBemJKeGh5NnZnVUQxMW51VEhGY3JteGR0VDJOIiwiZXhwIjoxNDg0ODE2MjIwLCJpYXQiOjE0ODQ3ODAyMjB9.RLuD2PC9yEDyiN5Wv-mXQPManoyFms3nFGwKxo2IvXo\\\",\\\"bucket\\\" : \\\"static.1989generationinitiative.org\\\",\\\"key\\\" : \\\"data/test.json\\\",\\\"body\\\" : \\\"Hello world with JAVA\\\"}\"\n" +
-                "}";
+        Map<String, String> request = new HashMap<>();
+        request.put("body", "{\"token\" : \"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3JpY2FyZDBqYXZpZXIuZXUuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDU4NzJiMTVjYmY4ZTYzMzI1MDY1OWEwZSIsImF1ZCI6IlIyZ1RBemJKeGh5NnZnVUQxMW51VEhGY3JteGR0VDJOIiwiZXhwIjoxNDg0ODE2MjIwLCJpYXQiOjE0ODQ3ODAyMjB9.RLuD2PC9yEDyiN5Wv-mXQPManoyFms3nFGwKxo2IvXo\",\"bucket\" : \"static.1989generationinitiative.org\",\"key\" : \"data/test.json\",\"body\" : \"Hello world with JAVA\"}");
         Context context = getContext();
         target.handleRequest(request, context);
 
