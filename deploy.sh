12915d085599b6a2013bdd260d44aa60fc3ad0b3
Ricardo Villanueva <villanueva.ricardo@gmail.com>
2017-01-21 01:36:49 +0000

    adding alias creation

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,11 @@ aws lambda create-function \
     --function-name 1989generationinitiative_uploader \
     --zip-file fileb://./build/distributions/1989generationInitiative.org_uploader-1.0.zip \
     --role arn:aws:iam::430132907316:role/initiativeRole \
-    --handler org.generationinitiative.uploader.Hello \
+    --handler org.generationinitiative.uploader.Hello::handleRequest \
     --runtime java8 \
     --environment Variables={JWT_CLIENT_SECRET=To_Be_Replaced}
 
+aws lambda create-alias \
+    --function-name 1989generationinitiative_uploader \
+    --name 89ers_uploader_alias \
+    --function-version \$LATEST
